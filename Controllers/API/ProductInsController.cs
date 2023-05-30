aa1bb0545589d1d0790a655efdaec5ee1c9efe1b
ferchojoshua <40898768+ferchojoshua@users.noreply.github.com>
2023-05-30 00:54:46 -0600

    Update ProductInsController.cs

--- Controllers/API/ProductInsController.cs
+++ Controllers/API/ProductInsController.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Claims;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
