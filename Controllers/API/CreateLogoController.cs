35e23a6522560a72c764c6d0f2571a37eabd34c3
FerchoJoshua <Ferchojoshua@gmail.com>
2024-09-06 00:35:27 -0600

    MEJORAS

--- Controllers/API/CreateLogoController.cs
+++ Controllers/API/CreateLogoController.cs
@@ -96,6 +96,71 @@ namespace Store.Controllers.API
 
 
 
+        [HttpPost("UpdateLogo")]
+        public async Task<ActionResult<UpdateLogo>> UpdateLogo([FromForm] UpdateLogoViewModel model)
+        {
+            string email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+            if (email == null)
+            {
+                return Unauthorized();
+            }
+
+            User user = await _userHelper.GetUserByEmailAsync(email);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            if (user.IsDefaultPass)
+            {
+                return Ok(user);
+            }
+
+            string token = HttpContext.Request.Headers["Authorization"].ToString();
+            token = token["Bearer ".Length..].Trim();
+            if (user.UserSession.UserToken != token)
+            {
+                await _userHelper.LogoutAsync(user);
+                return Ok("eX01");
+            }
+
+            if (!await _userHelper.IsAutorized(user.Rol, "MISCELANEOS CREATE"))
+            {
+                return Unauthorized();
+            }
+
+            try
+            {
+                if (model.Imagen == null || model.Imagen.Length == 0)
+                {
+                    return BadRequest("La imagen es requerida.");
+                }
+
+                byte[] imageBytes;
+                using (var ms = new MemoryStream())
+                {
+                    await model.Imagen.CopyToAsync(ms);
+                    imageBytes = ms.ToArray();
+                }
+
+                var result = await _createLogoHelper.UpdateLogoAsync(
+                    model.StoreId,
+                    model.Direccion,
+                    model.Ruc,
+                    imageBytes,
+                    model.Telefono,
+                    model.TelefonoWhatsApp
+                );
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
         [HttpGet("GetLogoByStoreId/{storeId}")]
         public async Task<ActionResult<GetLogoViewModel>> GetLogoByStoreId(int storeId)
         {
--- Program.cs
+++ Program.cs
@@ -35,19 +35,19 @@ if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development
 {
     builder.Services.AddDbContext<DataContext>(
         opt =>
-            opt.UseSqlServer(
+                    opt.UseSqlServer(
                 builder.Configuration.GetConnectionString("DevConnetion"),
                 x => x.UseNetTopologySuite()
             )
-    // opt.UseSqlServer(
-    //     builder.Configuration.GetConnectionString("MigConnetion"),
-    //     x => x.UseNetTopologySuite()
-    // )
-    // opt.UseSqlServer(
-    //     builder.Configuration.GetConnectionString("LocalConn"),
-    //     x => x.UseNetTopologySuite()
-    // )
-    );
+            //    // opt.UseSqlServer(
+            //    //     builder.Configuration.GetConnectionString("MigConnetion"),
+            //    //     x => x.UseNetTopologySuite()
+            //    // )
+            //opt.UseSqlServer(
+            //    builder.Configuration.GetConnectionString("ProdConnetion"),
+            //    x => x.UseNetTopologySuite()
+            //)
+            );
 }
 else
 {
@@ -99,7 +99,6 @@ builder.Services.AddScoped<IProductsInHelper, ProductsInHelper>();
 builder.Services.AddScoped<IProductHelper, ProductHelper>();
 builder.Services.AddScoped<IProductMovementsHelper, ProductMovementsHelper>();
 builder.Services.AddScoped<IClientsHelper, ClientsHelper>();
-builder.Services.AddScoped<ICreateLogoHelper, CreateLogoHelper>();
 builder.Services.AddScoped<ILocationsHelper, LocationsHelper>();
 builder.Services.AddScoped<ISalesService, SalesService>();
 builder.Services.AddScoped<IDashboardService, DashboardService>();
@@ -111,6 +110,8 @@ builder.Services.AddScoped<IReportsHelper, ReportsHelper>();
 builder.Services.AddScoped<IFacturationHelper, FacturationHelper>();
 builder.Services.AddScoped<IStoreHelper, StoreHelper>();
 builder.Services.AddScoped<IServerService, ServerService>();
+builder.Services.AddScoped<ICreateLogoHelper, CreateLogoHelper>();
+
 
 builder.Services.AddHostedService<Worker>().AddSingleton<IWorkerService, WorkerService>();
 
@@ -122,10 +123,10 @@ builder.Services.AddCors(options =>
         builder =>
         {
             builder
-                .WithOrigins("http://automoto.eastus.cloudapp.azure.com", "http://localhost:3000")
                 .AllowAnyHeader()
                 .AllowAnyMethod()
-                .AllowCredentials();
+                .AllowCredentials()
+                .WithOrigins("http://automoto.eastus.cloudapp.azure.com", "http://localhost:3000");
         }
     );
 });
